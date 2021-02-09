8e188eb75b80ecf623016d2602bc60a9b8907734
mllxmegan <74051439+mllxmegan@users.noreply.github.com>
2021-02-09 16:54:52 +0100

    ajout du code

---
+++ code/assets/js/script.js
@@ -0,0 +1,10 @@
+// TABLEAUX RESPONSIVE
+document.querySelectorAll('.table-responsive').forEach(function (table) {
+    let labels = Array.from(table.querySelectorAll('th')).map(function (th) {
+        return th.innerText
+    })
+    table.querySelectorAll('td').forEach(function (td, i) {
+        td.setAttribute('data-label', labels[i % labels.length])
+    })
+});
+
